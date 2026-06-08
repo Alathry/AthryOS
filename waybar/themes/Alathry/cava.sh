07282857579ace0c73b2dc7d4119903105a65fac
AlathrM7 <Alathry@proton.me>
2026-06-08 18:47:20 +0200

    Update cava.sh

--- waybar/themes/Alathry/cava.sh
+++ waybar/themes/Alathry/cava.sh
@@ -14,10 +14,7 @@ raw_target = /dev/stdout
 data_format = ascii
 ascii_max_range = 7
 EOF
-
-# قتل Cava بأمان عند إغلاق Waybar
 trap 'pkill -P $$ 2>/dev/null || true' EXIT
-
 bars=(▁ ▂ ▃ ▄ ▅ ▆ ▇ █)
 empty_bar="▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
 
@@ -33,19 +30,14 @@ convert_to_bars() {
     done
     printf '%s\n' "$out"
 }
-
-# قراءة المخرجات مع مؤقت زمني (0.3 ثانية)
 cava -p "$config_file" | while :; do
     if read -t 0.3 -r line; then
-        # هناك صوت وcava يرسل بيانات
         convert_to_bars "$line"
     else
         exit_status=$?
         if (( exit_status > 128 )); then
-            # انتهت المهلة (Timeout) بسبب سكون كرت الصوت، اطبع الشريط الثابت فوراً
             printf '%s\n' "$empty_bar"
         else
-            # Cava توقف عن العمل بالكامل
             break
         fi
     fi
